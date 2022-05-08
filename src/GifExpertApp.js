a97ec1bdadc74bf822fd6c5725fab544d4a0daa9
Tower95 <torrestmi3@gmail.com>
2022-05-07 18:25:41 -0700

    adding firts component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  ReactDOM  from "react";
+
+const GifExpertApp = ({})=>{
+  return(<>
+        <h2>Gif Expert App</h2> 
+        <hr></hr>
+        </>
+  );
+}
+
+export default GifExpertApp;
