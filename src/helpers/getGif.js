58bee16259eb222297f7347199ecef27024765a5
Tower95 <torrestmi3@gmail.com>
2022-10-24 20:14:27 -0700

    formating

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,16 +1,21 @@
 export const getGif = async (search, limit = 10) => {
 
   let apy_key = 'jJLZKM3OnTkgYCQrl05B39jI2s0bKDLl'
+
   let url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(search)}&limit=${limit}&api_key=${apy_key}`
+
   const resp = await fetch(url);
+
   let { data } = await resp.json();
 
   const gifs = data.map(({ title, id, images }) => {
+
     return {
       id,
       title,
       url: images.downsized_medium.url
     }
+
   })
 
   return gifs
